b46e1b20bec520166e3933d5afe8071f3b4f2f9b
manes08 <101406824+manes08@users.noreply.github.com>
2022-03-25 14:16:41 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    if (input.lightLevel() < 30) {
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        basic.showString("kaixo ")
+    }
+})
